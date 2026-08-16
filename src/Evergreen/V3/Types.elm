0bfa0ad04f6ae0d8d0486a4693138000fda286f3
Martin Stewart <MartinSStewart@gmail.com>
2026-08-16 23:26:37 +0200

    Preparing for v3

---
+++ src/Evergreen/V3/Types.elm
@@ -0,0 +1,41 @@
+module Evergreen.V3.Types exposing (..)
+
+import Browser
+import Lamdera
+import Url
+
+
+type alias FrontendModel =
+    { received : String
+    }
+
+
+type alias CountToFrontendState =
+    { count : Int
+    , clientId : Lamdera.ClientId
+    , busyWork : Int
+    }
+
+
+type alias BackendModel =
+    { countToFrontendState : Maybe CountToFrontendState
+    }
+
+
+type FrontendMsg
+    = UrlClicked Browser.UrlRequest
+    | UrlChanged Url.Url
+    | PressedCountToBackend
+    | NoOpFrontendMsg
+
+
+type ToBackend
+    = CountToBackendRequest
+
+
+type BackendMsg
+    = CountToFrontendStep
+
+
+type ToFrontend
+    = CountToFrontend Int
